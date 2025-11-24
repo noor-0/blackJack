35b343161ea3b22a27dd4943f332d5bf4006ce11
mdnoor-0 <md.noor.hassnain.01@gmail.com>
2025-11-24 13:08:30 +0530

    modified restart and added money

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const Spade = "♠";
-const Heart = "♥";
-const Diamond = "♦";
-const Club = "♣";
 const deck = [
   { name: "A♠", value: 11 }, { name: "2♠", value: 2 }, { name: "3♠", value: 3 }, { name: "4♠", value: 4 }, { name: "5♠", value: 5 }, { name: "6♠", value: 6 },{ name: "7♠", value: 7 }, { name: "8♠", value: 8 }, { name: "9♠", value: 9 },{ name: "10♠", value: 10 }, { name: "J♠", value: 10 }, { name: "Q♠", value: 10 },{ name: "K♠", value: 10 },{ name: "A♥", value: 11 }, { name: "2♥", value: 2 }, { name: "3♥", value: 3 },{ name: "4♥", value: 4 }, { name: "5♥", value: 5 }, { name: "6♥", value: 6 },{ name: "7♥", value: 7 }, { name: "8♥", value: 8 }, { name: "9♥", value: 9 },{ name: "10♥", value: 10 }, { name: "J♥", value: 10 }, { name: "Q♥", value: 10 },{ name: "K♥", value: 10 },{ name: "A♣", value: 11 }, { name: "2♣", value: 2 }, { name: "3♣", value: 3 },{ name: "4♣", value: 4 }, { name: "5♣", value: 5 }, { name: "6♣", value: 6 },{ name: "7♣", value: 7 }, { name: "8♣", value: 8 }, { name: "9♣", value: 9 },{ name: "10♣", value: 10 }, { name: "J♣", value: 10 }, { name: "Q♣", value: 10 },{ name: "K♣", value: 10 },{ name: "A♦", value: 11 }, { name: "2♦", value: 2 }, { name: "3♦", value: 3 },{ name: "4♦", value: 4 }, { name: "5♦", value: 5 }, { name: "6♦", value: 6 },{ name: "7♦", value: 7 }, { name: "8♦", value: 8 }, { name: "9♦", value: 9 },{ name: "10♦", value: 10 }, { name: "J♦", value: 10 }, { name: "Q♦", value: 10 },{ name: "K♦", value: 10 },
 ];
 let playerScore = 0
 let dealerScore = 0
+let pmoney = 145
 let usedCard = new Set();
+
+document.querySelector("#ht").style.display = 'none'
+document.querySelector('#std').style.display = 'none'
+document.querySelector('#refh').style.display = 'none'
 
 function getCard() {
     let rnd = Math.floor(Math.random() * 52); 
@@ -56,37 +57,63 @@ function hit(){
     if(playerScore>22) findWinner();
 }
 function start(){
+    document.querySelector('#money').textContent = 'Per: $' + pmoney
+    document.querySelector("#strt").style.display = 'none'
+    document.querySelector("#std").style.display = 'block'
+    document.querySelector('#ht').style.display = 'block'
+    addCardP();
+    addCardP();
     addHiddenCard();
     addCardD();
-    addCardP();
-    addCardP();
     if(playerScore>22 || dealerScore>22)findWinner();
 }
 const hed = document.getElementById('heading');
 const won = "You Won!";
 const lost = "You Lost!";
-const tie = "Tie!"
+const tie = "Tie!";
+const nomoney = "No Coins!"
 function findWinner(){
+    flagWon = true
+    flagtie = false;
     document.getElementById('hidden-card').style.backgroundColor = 'white';
     if(playerScore > 22){
-        hed.textContent = lost;
+        flagWon = false;
     }
-    else if(dealerScore > 22){
-        hed.textContent = won;
-    }
+    else if(dealerScore > 22);
     else if(playerScore == dealerScore){
+        flagtie = true;
+    }
+    else if(playerScore < dealerScore) flagWon = false;
+    if(flagtie){
         hed.textContent = tie;
     }
-    else if(playerScore > dealerScore){
+    else if(flagWon){
         hed.textContent = won;
+        pmoney += 25;
     }
-    else hed.textContent = lost;
-    
+    else{
+        hed.textContent = lost;
+        pmoney -= 20;
+    }
 
+    document.querySelector('#money').textContent = 'Per: $' + pmoney
     document.getElementById("ht").style.display = 'none';
     document.getElementById('std').style.display = 'none';
     document.getElementById("strt").style.display = 'none';
+    document.querySelector("#refh").style.display = 'block'
 }
 function refresh(){
-    window.location.reload();
+    usedCard.clear();
+    const pd = document.querySelector('.card-div-d');
+    const dd = document.querySelector('.card-div-p');
+    pd.innerHTML = ''
+    dd.innerHTML = ''
+    playerScore = 0
+    dealerScore = 0
+    if(pmoney < 20){
+        hed.textContent = nomoney;
+        return;
+    }
+    document.querySelector('#refh').style.display = 'none'
+    document.querySelector('#strt').style.display = 'block'
 }
